144b24fb5bd98296214cb24f60b07e93bfe6c330
Jeremy RICHARD <jrichard@e1r2p11.42.fr>
2017-04-14 16:02:34 +0200

    deleted forbidden function in realloc

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,7 @@
 /*   By: jrichard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/09 11:26:53 by jrichard          #+#    #+#             */
-/*   Updated: 2017/01/17 01:30:54 by jrichard         ###   ########.fr       */
+/*   Updated: 2017/04/14 16:02:19 by jrichard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ void		*ft_realloc(void *ptr, size_t size)
 {
 	void	*new;
 
-	printf("%zu\n", size);
 	if (!(new = malloc(size)))
 		return (NULL);
 	if (ptr)
